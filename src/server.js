6aac13d8b589efbce4de19ef35d4ff70412b02f5
Alexey <makovii88@gmail.com>
2025-10-29 17:42:26 +0100

    update base page

--- src/server.js
+++ src/server.js
@@ -25,6 +25,12 @@ app.use(
   }),
 );
 
+app.get('/', (req, res) => {
+  res.status(200).json({
+    message: 'Hello Node.js',
+  });
+});
+
 app.get('/notes', (req, res) => {
   res.status(200).json({ message: 'Retrieved all notes' });
 });
