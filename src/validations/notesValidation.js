8446c4dcc15d7e60a57163708970e2eca8714269
Alexey <makovii88@gmail.com>
2025-11-11 16:53:53 +0100

    update updateNoteSchema, noteId & errors

--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -17,7 +17,7 @@ export const getAllNotesSchema = {
 
 export const noteIdSchema = {
   [Segments.PARAMS]: Joi.object({
-    notesId: Joi.string().custom(objectIdValidator).required(),
+    noteId: Joi.string().custom(objectIdValidator).required(),
   }),
 };
 
@@ -32,7 +32,7 @@ export const createNoteSchema = {
 export const updateNoteSchema = {
   ...noteIdSchema,
   [Segments.BODY]: Joi.object({
-    title: Joi.string().min(1).required(),
+    title: Joi.string().min(1).min(1),
     content: Joi.string().allow(''),
     tag: Joi.string().valid(...TAGS),
   }).min(1),
