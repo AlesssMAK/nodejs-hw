f0b6f0b3c34ad52a2d09da06a981b6cc389386cc
Alexey <makovii88@gmail.com>
2025-11-17 21:45:57 +0100

    add register user

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ import notesRoutes from './routes/notesRoutes.js';
 import { connectMongoDB } from './db/connectMongoDB.js';
 import { errors } from 'celebrate';
 import helmet from 'helmet';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
@@ -16,6 +17,7 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(logger);
+app.use(cookieParser());
 
 app.use(notesRoutes);
 
